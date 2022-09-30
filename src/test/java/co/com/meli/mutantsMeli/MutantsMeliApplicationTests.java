de819bba406d5db36f9b450b77f17510b64e34a7
ls.cortezgiraldo <ls.cortezgiraldo@gmail.com>
2022-09-29 21:04:36 -0500

    create project SpringBoot Initializer

---
+++ src/test/java/co/com/meli/mutantsMeli/MutantsMeliApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.meli.mutantsMeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantsMeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
